06b4d211103d37565d619f63ec2f66e104089b29
hookkl21 <hookkl21@gmail.com>
2022-11-29 13:00:41 -0500

    Update About section

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About() {
     >
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1200px] w-full grid grid-cols-2 gap-6">
-          <div className="sm:text-right pb-8 pl4 mx-6">
+          <div className="sm:text-left py-8 pl4 mx-6">
             <p className="text-4xl font-bold inline border-b-4 border-indigo-600">
               About
             </p>
@@ -21,21 +21,20 @@ function About() {
             <img
               src={profile}
               alt="Masaki Hook"
-              className="rounded-full sm:mt-4 mx-auto w-[75%]"
+              className="rounded-full sm:mt-4 mx-auto w-[65%]"
             />
           </div>
-          <div>
+          <div className="pb-6">
             <p className="mx-3 pt-3 mt-4 text-1xl sm:tracking-wider leading-8">
               I am a Bachelor's of Computer Science Graduate from UCF
               (University of Central Florida). Although I have a relative
-              degree, I have self studied in Front-End Web development concepts,
-              such as CSS/HTML, JavaScript programming languages, React.js
-              libralies, and Bootstrap/Tailwind css frameworks to name a few.
-              Having a baseball background of 20 years, it helps me to keep
-              myself motivated, and be able to practice my skills efficiently to
-              improve myself in tech. I am determined to deliver advanced and
-              efficient products to the team and clients. Blessed to be able to
-              speak Japanese and English fluetly as a bonus.
+              degree, I have self studied in Front-End/Web and HTML Email
+              development concepts, such as responsive HTML/CSS email template,
+              JavaScript, React.js libralies, and Bootstrap/Tailwind css
+              frameworks to name a few. Having a baseball background of 20
+              years, I keep myself motivated, and practice essential skills
+              daily to improve myself to stay on top of any technical
+              advancements.
             </p>
           </div>
         </div>
